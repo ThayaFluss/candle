955bedb41855cdf940d9980adff6badf3ca2a554
ecureuil <hayafluss@gmail.com>
2020-08-19 14:57:13 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Hayes@thayafluss", # Replace with your own username
+    name="candle", 
     version="1.0.1",
     author="Hayes",
     author_email="hayfluss@gmail.com",
