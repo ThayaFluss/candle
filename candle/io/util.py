571a7feb20fb672aeb3d2af574a5a1dd34a1917b
ecureuil <hayafluss@gmail.com>
2020-08-19 13:51:12 +0900

    fix io.util

--- candle/io/util.py
+++ candle/io/util.py
@@ -11,26 +11,6 @@ def touch(path):
 
 
 
-import yaml
-import time, datetime
-
-
-
-def write_config(dirname, additional_args):
-    os.makedirs(dirname, exist_ok=True)
-    with open("{}/config.yml".format(dirname), "w" ) as f:
-        yaml.dump(additional_args,f)
-    return
-
-def get_plot_dir(args):
-    """
-    TODO:Duplicated
-    """
-    today = datetime.datetime.fromtimestamp(time.time())
-    t = today.strftime('%Y%m%d%H%M%S')
-    dirname = "plot/{}_intvl-{}_L-{}_{}".format(args.net, args.interval, args.L,t)
-    return dirname
-
 
 
 
