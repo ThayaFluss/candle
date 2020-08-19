e454f7ac6ebebd868586f530e0cdbe19728b60ec
ecureuil <hayafluss@gmail.com>
2020-08-19 14:41:12 +0900

    update setup

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Hayes@thayafluss", # Replace with your own username
-    version="0.0.1",
+    version="1.0.1",
     author="Hayes",
     author_email="hayfluss@gmail.com",
     description="Toolkit for Pytorch",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ThayaFluss/candle",
-    packages=setuptools.find_packages(),
+    package_dir={"": "candle"},
+    packages=setuptools.find_namespace_packages(where="candle"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
